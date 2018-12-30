d8210274f1b7ac0fc34400897759793ed7328a13
patrick piemonte <patrick.piemonte@gmail.com>
2018-12-29 23:54:37 -0800

    add a proper framework target https://github.com/NextLevel/NextLevelSessionExporter/issues/12

---
+++ NextLevelSessionExporter/Framework/NextLevelSessionExporter.h
@@ -0,0 +1,15 @@
+//
+//  NextLevelSessionExporter.h
+//  NextLevelSessionExporter
+//
+//  Created by Patrick Piemonte on 12/29/18.
+//  Copyright © 2018 Next Level. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NextLevelSessionExporter.
+FOUNDATION_EXPORT double NextLevelSessionExporterVersionNumber;
+
+//! Project version string for NextLevelSessionExporter.
+FOUNDATION_EXPORT const unsigned char NextLevelSessionExporterVersionString[];
